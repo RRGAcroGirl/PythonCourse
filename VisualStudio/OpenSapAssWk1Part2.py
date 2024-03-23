6eb9c2fd2179beb816a8ed1f478b4cfb2e48c60d
Nicole Meyer <meyer.k.nicole@gmail.com>
2024-03-22 20:26:35 -0400

    Edited to match CodeOcean requirements.

--- VisualStudio/OpenSapAssWk1Part2.py
+++ VisualStudio/OpenSapAssWk1Part2.py
@@ -3,12 +3,15 @@ angle_2 = int(input("Please enter the degreee of angle 2 of your triangle:"))
 angle_3 = int(input("Please enter the degreee of angle 3 of your triangle:"))
 
 angle_sum = angle_1 + angle_2 + angle_3
-if (angle_1 > 0) and (angle_2 > 0) and (angle_3 > 0) and (angle_sum == 180):
+if angle_sum != 180:
+    print("The entered values are not valid.")
+
+elif (angle_1 > 0) and (angle_2 > 0) and (angle_3 > 0):
     if (angle_1 == 90) or (angle_2 == 90) or (angle_3 == 90):
-        print("You have a right triangle.")
+        print("The triangle is a right triangle.")
     if (angle_1 > 90) or (angle_2 > 90) or (angle_3 > 90):
-        print("You have an obtuse triangle.")
+        print("The triangle is an obtuse triangle.")
     if (angle_1 < 90) and (angle_2 < 90) and (angle_3 < 90):
-        print("You have a acute triangle.")
+        print("The triangle is an acute triangle.")
 else:
-    print("Sorry, those values are not valid. Please check your angles and try again.")
+    print("Angles smaller than 0 are not valid.")
